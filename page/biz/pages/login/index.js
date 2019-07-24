8c6a3d3cd3cbe5f70f2a3ee5a3fd6e917c1da591
2892549534KIKL <2658474725@qq.com>
2019-07-24 19:34:29 +0800

    kikl

--- page/biz/pages/login/index.js
+++ page/biz/pages/login/index.js
@@ -3,7 +3,6 @@ Page({
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
     if(e.detail.value.name=='')
     {alert('用户名不能为空');}
-      if(e.detail.value.password=='')
-    {alert('密码不能为空');}
+  
   }
 })
