f439a6dab2d5f36dfa6fcaaa5830d2ba61c6f5e3
2239652215 <2239652215@qq.com>
2019-08-02 16:23:40 +0800

    修改了免登

--- pages/login/login.js
+++ pages/login/login.js
@@ -6,7 +6,7 @@ Page({
     var that = this;
     //1:获取token
     dd.httpRequest({
-      url: 'http://39.96.30.233/zjp/DD_API/getAccess_token',
+      url: 'http://39.96.30.233/zjp/BusinessReport_DD_API/getAccess_token',
       method: 'get',
       dataType: 'json',
       success: function(res) {
@@ -27,7 +27,7 @@ Page({
               headers: {
                 "Content-Type": "application/string"
               },
-              url: 'http://39.96.30.233/zjp/DD_API/getDDUser',
+              url: 'http://39.96.30.233/zjp/BusinessReport_DD_API/getDDUser',
               method: 'POST',
               data: (that.data.authCode),
               success: function(res) {
@@ -36,44 +36,20 @@ Page({
                   userid: JSON.parse(res.data).userid,
                   userName: JSON.parse(res.data).name,
                 })
-                console.log('获取成功userid' + that.data.userid);
-                //获取后查询是否此钉钉id是否已绑定账号
-                dd.httpRequest({
-                  headers: {
-                    "Content-Type": "application/json"
-                  },
-                  url: 'http://39.96.30.233/zjp/users/findByDdUserId',
-                  method: 'POST',
-                  data: (
-                    that.data.userid
-                  ),
-                  dataType: 'json',
-                  success: function(res) {
-                    console.log('登录后数据:' + JSON.stringify(res.data.data));
-                    //已绑定账号
-                    if (res.data.code > 0) {
-                      dd.setStorage({
-                        key: 'user',
-                        data: {
-                          user: res.data.data,
-                        }
-                      });
-                      dd.switchTab({
-                        url: '../index/index',
-                      })
-                    }
-                    //未绑定账号
-                    else {
-                      dd.alert({ content: JSON.stringify(res.data.msg) + ",请先绑定哦" });
-                    }
-                  }, complete: function(res) {
-                    dd.hideLoading();
-                  }
-                });
+                if (that.data.userid == undefined) {
+                  dd.alert({
+                    content: "免登失败,请登陆后重试"
+                  })
+                }else{
+                  dd.navigateTo({ url: '../index/index' });
+                }
+                dd.hideLoading();
+                console.log('获取成功userid:  ' + that.data.userid);
               }
             })
           },
           fail: function(err) {
+            dd.hideLoading();
           }
         });
       }, fail: function(err) {
@@ -95,77 +71,5 @@ Page({
     userid: null,//用户id
     userName: null,//用户钉钉名称
   },
-  // 表单提交
-  onSubmit(e) {
-    var that = this;
-
-    if (e.detail.value.name == "") {
-      dd.showToast({ content: '请输入账号', duration: 2000 });
-      return;
-    } else if (e.detail.value.password == "") {
-      dd.showToast({
-        content: '请输入密码',
-        duration: 2000,
-      });
-      return;
-    }
-    setTimeout(() => {
-      dd.showLoading({
-        content: '加载中...',
-        delay: 100,
-      });
-      dd.httpRequest({
-        headers: {
-          "Content-Type": "application/json"
-        },
-        url: 'http://39.96.30.233/zjp/users/login',
-        method: 'POST',
-        data: JSON.stringify({
-          name: e.detail.value.name,
-          password: e.detail.value.password,
-          ddUserId: that.data.userid,
-        }),
-        dataType: 'json',
-        success: function(res) {
-          if (res.data.code > 0) {
-            dd.setStorage({
-              key: 'user',
-              data: {
-                user: res.data.data
-              }
-            });
-            dd.hideLoading();
-            dd.switchTab({
-              url: '../index/index',
-            })
-          }
-          else {
-            dd.hideLoading();
-            dd.showToast({ content: res.data.msg, duration: 3000, });
-          }
-        },
-      });
-      //   dd.getStorage({
-      //     key: 'user',
-      //     success: function(res) {
-      //       console.log(res)
-      //       dd.alert({content: '获取成功：' + res.data.user.actorName});
-      //     },
-      //     fail: function(res){
-      //       dd.alert({content: res.errorMessage});
-      //     },
-      // });
-    });
-  },
-  // 跳转页面
-  // navigate(e) {
-  //   const { url, openType = 'navigateTo' } = e.currentTarget.dataset;
-  //   my[openType]({ url });
-  // },
-  //  loginSucceed(e){
-  //   dd.reLaunch({
-  //     url: '../../pages/myAudit/myAudit',
-  //   });
-  // }
 
 });
