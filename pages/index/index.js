67190a79db2fe756cc26da466e5578955e55f5ff
2239652215 <2239652215@qq.com>
2019-11-29 08:26:43 +0800

    加了一个temp页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,19 @@
 Page({
-    data: {
-      checkedIn:199,//已签到人数
-      NoCheckedIn:30,//已签到人数
-      exceptions:5,//异常人数
-      topThreeImage:['../images/no1.png','../images/no2.png','../images/no3.png'],//前三签到图标
-      topThreeSign:[//前三签到数据
-        { name: '张三', time: '08:35' },
-        { name: '李四', time: '08:36' },
-        { name: '王五', time: '08:37' },
-      ],
-      signData:[
-        { name: '张三', time: '08:35',number: 2 ,site:'福建省福州市鼓楼区安泰街道福州鼓楼新高达教育培训中心(南门校区)新兴大厦(斗西路)'},
-        { name: '李四', time: '08:36',number: 1 ,site:'江西省赣州市章贡区黄金岭街道迎宾大道辅路赣州富翔斯巴鲁汽车销售服务有限公司'},
-        { name: '王五', time: '08:37',number: 3 ,site:'福建省福州市鼓楼区湖东路140号'},
-      ],
+  data: {
+    checkedIn: 199,//已签到人数
+    NoCheckedIn: 30,//已签到人数
+    exceptions: 5,//异常人数
+    topThreeImage: ['../images/no1.png', '../images/no2.png', '../images/no3.png'],//前三签到图标
+    topThreeSign: [//前三签到数据
+      { name: '张三', time: '08:35' },
+      { name: '李四', time: '08:36' },
+      { name: '王五', time: '08:37' },
+    ],
+    signData: [
+      { name: '张三', time: '08:35', number: 2, site: '福建省福州市鼓楼区安泰街道福州鼓楼新高达教育培训中心(南门校区)新兴大厦(斗西路)' },
+      { name: '李四', time: '08:36', number: 1, site: '江西省赣州市章贡区黄金岭街道迎宾大道辅路赣州富翔斯巴鲁汽车销售服务有限公司' },
+      { name: '王五', time: '08:37', number: 3, site: '福建省福州市鼓楼区湖东路140号' },
+    ],
   },
   onLoad(query) {
     // 页面加载
